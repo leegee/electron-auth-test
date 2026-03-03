dace60eda8c4566e17731d79eb5b068c7b6eb5fd
Lee Goddard <leegee@gmail.com>
2026-03-03 15:17:14 +0100

    Remove listener once used

--- src/preload.cjs
+++ src/preload.cjs
@@ -2,5 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     loginGitHub: () => ipcRenderer.send('login-github'),
-    onOAuthSuccess: (callback) => ipcRenderer.on('oauth-success', callback)
+    onOAuthSuccess: (callback) => {
+        const wrapper = (_event, ...args) => {
+            callback(...args);
+            ipcRenderer.removeListener('oauth-success', wrapper);
+        };
+        ipcRenderer.on('oauth-success', wrapper);
+    }
 });
