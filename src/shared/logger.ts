17495c5892ef20a6dbdaade6dd14be6e2cf89840
Lee Goddard <leegee@gmail.com>
2026-03-09 18:22:27 +0100

    avoid

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,6 +1,13 @@
 import 'electron-log/preload';
 import log from 'electron-log';
 
+// Avoid duplicate log messages in Vite dev mode
+if (typeof window !== 'undefined' && !(window as any).__electronLogInitialized) {
+    (window as any).__electronLogInitialized = true;
+    log.transports.console.level = false;
+}
+
+
 if (typeof process !== 'undefined' && (process as any).type === 'browser') {
     const fs = require('fs');
 
