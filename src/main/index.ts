21b9c58984e445454e2ec46408abe3a1eee9f216
Lee Goddard <leegee@gmail.com>
2026-03-09 11:09:56 +0100

    Modern custom protocol api

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,7 @@
 // src\main\index.ts
-import { app, shell, BrowserWindow, ipcMain, protocol } from 'electron';
-import path from 'path';
+import { app, BrowserWindow, ipcMain, net, protocol, shell } from 'electron';
+import path from 'node:path';
+import url from 'node:url';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 
 import icon from '../../resources/icon.png?asset';
@@ -100,16 +101,51 @@ function createWindow(): BrowserWindow {
 
 // Register custom protocol for deep links
 function registerCustomProtocol() {
-  try {
-    protocol.registerFileProtocol(config.VITE_CUSTOM_URL_PROTOCOL, (request, callback) => {
-      const urlPath = request.url.replace(`${config.VITE_CUSTOM_URL_PROTOCOL}://`, '');
-      const filePath = path.join(app.getAppPath(), 'dist', 'renderer', urlPath);
-      console.log(`Serving ${config.VITE_CUSTOM_URL_PROTOCOL}:// -> ${filePath}`);
-      callback({ path: filePath });
-    });
-  } catch (err) {
-    console.error(`Failed to register "${config.VITE_CUSTOM_URL_PROTOCOL}" protocol`, err);
-  }
+  // try {
+  //   protocol.registerFileProtocol(config.VITE_CUSTOM_URL_PROTOCOL, (request, callback) => {
+  //     const urlPath = request.url.replace(`${config.VITE_CUSTOM_URL_PROTOCOL}://`, '');
+  //     const filePath = path.join(app.getAppPath(), 'dist', 'renderer', urlPath);
+  //     console.log(`Serving ${config.VITE_CUSTOM_URL_PROTOCOL}:// -> ${filePath}`);
+  //     callback({ path: filePath });
+  //   });
+  // } catch (err) {
+  //   console.error(`Failed to register "${config.VITE_CUSTOM_URL_PROTOCOL}" protocol`, err);
+  // }
+  protocol.handle(config.VITE_CUSTOM_URL_PROTOCOL, (req: GlobalRequest) => {
+    try {
+      // let urlPath = req.url.replace(`${config.VITE_CUSTOM_URL_PROTOCOL}://`, '');
+
+      const { pathname } = new URL(req.url);
+      let urlPath = decodeURIComponent(pathname);
+
+      if (!urlPath || urlPath === '/') {
+        urlPath = 'index.html';
+      }
+
+      const base = path.join(app.getAppPath(), 'dist', 'renderer');
+      const filePath = path.resolve(base, '.' + urlPath);
+
+      // Prevent directory traversal
+      if (!filePath.startsWith(base)) {
+        return new Response('Forbidden', { status: 403 });
+      }
+
+      if (!path.extname(filePath)) {
+        return net.fetch(url.pathToFileURL(path.join(base, 'index.html')).toString());
+      }
+
+      console.log(`Serving ${config.VITE_CUSTOM_URL_PROTOCOL}://  ${filePath}`)
+      return net.fetch(url.pathToFileURL(filePath).toString());
+    }
+
+    catch (err) {
+      console.error(`Failed to serve "${req.url}"`, err)
+      return new Response('Internal error', {
+        status: 500,
+        headers: { 'content-type': 'text/plain' }
+      });
+    }
+  })
 }
 
 
